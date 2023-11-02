e403a6f89a0b0270370ca2a1638353eb01ccc543
Ayvenou <axel.yvenou@gmail.com>
2023-11-02 15:47:32 +0100

    init

---
+++ src/main/java/fr/learn/springsecurity6authserver/SpringSecurity6AuthServerApplication.java
@@ -0,0 +1,13 @@
+package fr.learn.springsecurity6authserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity6AuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity6AuthServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/learn/springsecurity6authserver/SpringSecurity6AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.learn.springsecurity6authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurity6AuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
